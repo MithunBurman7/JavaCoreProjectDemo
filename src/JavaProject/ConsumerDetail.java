8b2ff2e8ffdc80549ebc3ededdcb28c0e883777d
MithunBurman7 <mithunburman7@gmail.com>
2017-09-08 15:35:33 +0530

    Java Core Project

---
+++ src/JavaProject/ConsumerDetail.java
@@ -0,0 +1,10 @@
+package JavaProject;
+
+public class ConsumerDetail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/JavaProject/RetailStoreDemo.java
@@ -0,0 +1,10 @@
+package JavaProject;
+
+public class RetailStoreDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
